efd5e5d8773c1406de9892af4a405237e84f3be0
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2024-06-25 22:48:12 +0300

    findPose & findPosition

--- PoseDetectorModule.py
+++ PoseDetectorModule.py
@@ -5,7 +5,7 @@ import time
 
 class PoseDetector:
     def __init__(self, mode=False, complexity=1, smoothLm=True, seg=True, smoothSeg=True, detectionCon=0.5, trackCon=0.5):
-        """ 
+        """
             mode: In static images, set this to False. In videos, set this to True.
             complexity: The complexity of the pose model: 0, 1, or 2. 0 is the fastest, and 2 is the most accurate.
             smoothLm: Smooth the landmark points to reduce jitter.
@@ -27,3 +27,39 @@ class PoseDetector:
         self.pose = self.mpPose.Pose(self.mode, self.complexity, self.smoothLm,
                                      self.seg, self.smoothSeg, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+
+    def findPose(self, img, draw=True):
+        """
+            Args:
+                img: The image to process.
+                draw: Whether to draw the pose landmarks on the image.
+
+            Returns:
+                img: The image with or without the pose landmarks drawn on it.
+        """
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(
+                    img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, draw=True):
+        """ 
+            Args:
+                img: The image to process.
+                draw: Whether to draw the pose landmarks on the image.
+
+            Returns:
+                lmList: A list of the landmark points.
+        """
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return lmList
